5ef1d9c99bc66e5c3805f878a0115c2113e9685a
Chris Rigby <chris.rigby@onthebeach.co.uk>
2018-04-28 21:23:46 +0100

    improve logging

--- button.py
+++ button.py
@@ -30,7 +30,7 @@ class IftttWebHook:
         self.options = options
 
     def send_event(self, options = {}):
-        logging.debug('in %s', self.__class__.__name__)
+        logging.debug('in %s::%s', self.__class__.__name__,'send_event')
         webhook_endpoint = "%s/%s/with/key/%s" % (
                 self.webhook_endpoint,
                 self.event_name,
@@ -47,7 +47,7 @@ class DoorBell(object):
     soundfile = os.path.join(cwd, 'lib/sounds/bingbong.wav')
 
     def chime(self):
-        logging.debug('in %s', self.__class__.__name__)
+        logging.debug('in %s::%s', self.__class__.__name__,'chime')
         wf = wave.open(self.soundfile, 'rb')
         p = pyaudio.PyAudio()
         stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
@@ -77,16 +77,16 @@ class ButtonEvent(object):
     webhook = IftttWebHook(ifttt_webhook_endpoint,ifttt_webhook_key,'button_pressed')
 
     def get_button_pressed(self, hwsrc):
-        logging.debug('in %s', self.__class__.__name__)
+        logging.debug('in %s::%s', self.__class__.__name__,'get_button_pressed')
         logging.debug("Processing %s" % hwsrc)
         for button in self.buttons:
             if hwsrc in button['hwsrc']:
                 logging.info('%s button pressed' % button['name'])
                 self.webhook.send_event(options={'value1': button['name']})
-                self.doorbell.chime()
+                # self.doorbell.chime()
 
     def get_arp_src(self, packet):
-        logging.debug('in %s', self.__class__.__name__)
+        logging.debug('in %s::%s', self.__class__.__name__,'get_arp_src')
         hwsrc = packet[ARP].hwsrc
         self.get_button_pressed(hwsrc)
 
